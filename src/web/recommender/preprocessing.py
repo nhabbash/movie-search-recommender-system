f4f6330dcb286f0b9201f013e3a83be68defc2aa
ricardo_aragon <34106214+ricardoanibalmatamorosaragon@users.noreply.github.com>
2019-12-27 18:05:05 +0100

    update add user bow

--- src/web/recommender/preprocessing.py
+++ src/web/recommender/preprocessing.py
@@ -6,9 +6,11 @@ from nltk.corpus import stopwords
 from nltk.tokenize import word_tokenize
 from nltk.stem import PorterStemmer 
 import numpy as np
+from sklearn.feature_extraction.text import TfidfVectorizer
 
 dataset = pd.read_csv("./data/parsed_dataset.csv", delimiter = ";")
 ratings = pd.read_csv("./data/ratings_small.csv")
+users = pd.read_csv("./data/users.csv")
 
 #preprocessing overwies
 stop_words = set(stopwords.words('english'))
@@ -45,6 +47,7 @@ for i in list(dataset['overview'].values):
         i = remove_punctuation(i)
     corpus.append(' '.join(i))
 
+
 dataset['overv_pp'] = corpus
 dataset = dataset[['id','title','overv_pp','genres','spoken_languages','original_language']]
 
@@ -55,3 +58,33 @@ item_enc = LabelEncoder()
 complete_data['movieId'] = item_enc.fit_transform(complete_data['id'].values)
 
 complete_data.to_csv('./data/dataset_rs.csv', index= False)
+
+#create user final , add bow and history_films with ratings
+def create_bow_user(userId):
+    data = complete_data.query("userId == "+str(userId))[0:5]
+    tf_u = TfidfVectorizer()
+    tfidf_matrix_u = tf_u.fit_transform(data['overv_pp'])
+    user_bow = [tfidf_matrix_u.toarray()[0], tf_u.get_feature_names()]
+    weight=[]
+    term=[]
+    for i in range(len(user_bow[0])):
+        if user_bow[0][i] != 0:
+            weight.append(user_bow[0][i])
+            term.append(user_bow[1][i])
+    
+    user_bow = [weight,term]
+    return user_bow, list(data['id'].values), list(data['rating'].values)
+    
+bow_list = []
+film_list =[]
+for i in users['user_id'].values:
+    tmp = create_bow_user(i)
+    bow_list.append(tmp[0:1])
+    film_list.append(list(zip(tmp[1],tmp[2])))
+user_dic = {'history_films':film_list, 'bow':bow_list}
+
+user_dataframe = pd.DataFrame(user_dic)
+
+df = users.join(user_dataframe)
+
+df.to_csv('./data/users_final.csv',index = False)
